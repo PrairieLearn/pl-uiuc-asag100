100b4c569578959da63b4f046a0b263f8a2d1ead
Victor Zhao <victorsss.orz@outlook.com>
2024-12-11 14:26:43 -0600

    add short survey

---
+++ questions/Survey/Calculator/server.py
@@ -0,0 +1,11 @@
+def grade(data):
+    data["score"] = 1
+    data["partial_scores"]["q1"] = {"score": None}
+    data["partial_scores"]["q2"] = {"score": None}
+    data["partial_scores"]["q3"] = {"score": None}
+    data["partial_scores"]["q4"] = {"score": None}
+    data["partial_scores"]["q5"] = {"score": None}
+    data["partial_scores"]["q6"] = {"score": None}
+    data["partial_scores"]["q7"] = {"score": None}
+    data["partial_scores"]["q8"] = {"score": None}
+    data["partial_scores"]["q9"] = {"score": None}

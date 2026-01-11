383ee621765e801706e848844f30460c68fa32ec
Victor Zhao <victorsss.orz@outlook.com>
2026-01-11 15:31:43 -0600

    modify vignette, create assessment

--- questions/fairnessPerceptionsStudy/vignette/server.py
+++ questions/fairnessPerceptionsStudy/vignette/server.py
@@ -3,16 +3,15 @@ import random
 
 def generate(data):
     stakes_pairs = {
-        "high stakes": "will be included in a time exam that counts towards 35% of the overall grade",
-        "low stakes": "will be included in a one homework that counts towards 1% of the overall grade",
+        "high stakes": "on a final exam that counts towards 35% of the overall course grade",
+        "low stakes": "on a homework assignment that counts towards 1% of the overall course grade",
     }
     explanation_pairs = {
         "no explanation": """
-            The feedback states that the AI grader uses a hidden instructor-provided rubric to perform the grading:<br>
-            Your grade is 50%, which was computed by the AI based on a rubric defined by the instructor, which will be available to you after the deadline""",
+            The feedback states that the AI grader assigns a score using an instructor-defined rubric that is not shown to students.<br>
+            Your grade is 50%, which was computed by the AI based on a rubric defined by the instructor. The rubric will be available after the deadline.""",
         "rubric information": """
-            The feedback provides how the AI grader reaches its grading. 
-            It includes an instructor-provided rubric used during the grading process, along with the weight of each rubric item:
+            The feedback explains how the AI grader evaluated the response by listing the rubric items and their weights:
             <ul style="list-style-type: none;"> 
                 <li><i class="bi bi-check-square-fill" style="color: blue;"></i> which norm to use (50%) </li>
                 <li><i class="bi bi-square"></i> error definition (25%)</li>
@@ -20,7 +19,7 @@ def generate(data):
             </ul>
         """,
         "improvement suggestions": """
-            The feedback provides a list of things to consider/improve in order to receive a higher grade: 
+            The feedback includes specific suggestions for how the response could be improved to receive a higher score, such as: 
             <ul> 
                 <li>Draw connections between landscape changes and how we define numerical errors</li>
                 <li>Incorporate the problem context more when stating the meaning of a large norm</li>
@@ -28,23 +27,23 @@ def generate(data):
         """,
     }
     oversight_pairs = {
-        "ai": "Given the latest technological advances and in an effort to perform grading in a timely manner for better learning outcomes, the AI grades will be final.",
-        "human": "Given the latest technological advances and in an effort to provide feedback in a timely manner for better learning outcomes, the AI grader will provide a grade for reference, along with feedback for the student. A human TA will later manually check all the submissions and make a new grading while consulting the AI grades.",
+        "no": "The AI-assigned grade will not be manually reviewed by a human TA.",
+        "yes": "The AI-assigned grade will later be reviewed by a human TA and adjusted if necessary.",
     }
     resubmission_pairs = {
-        "no": "Students can not modify the submission once submitted to the AI.",
-        "yes": "Students can choose to modify the submission based on the feedback provided by the AI and resubmit to the AI for another round of grading.",
+        "no": "Students have only one opportunity to submit their response. After receiving AI feedback, they cannot modify or resubmit their answer.",
+        "yes": "Students may revise their responses based on the AI feedback and resubmit multiple times for additional rounds of AI grading.",
     }
     regrading_pairs = {
-        "no": "Students can not choose to ask a TA for a regrade on the last submission.",
-        "yes": "However, students can choose to ask a TA for a regrade on the last submission.",
+        "no": "	Students cannot request a regrade, and the AI-assigned score cannot be disputed.",
+        "yes": "After the assessment closes, students have one week to request a regrade by a human TA if they believe the AI-assigned score is inaccurate.",
     }
 
     stakes, stakes_text = random.choice(list(stakes_pairs.items()))
     explanation, explanation_text = random.choice(list(explanation_pairs.items()))
     oversight, oversight_text = random.choice(list(oversight_pairs.items()))
     resubmission, resubmission_text = random.choice(list(resubmission_pairs.items()))
-    if oversight == "ai":
+    if oversight == "no":
         regrading, regrading_text = random.choice(list(regrading_pairs.items()))
     else:
         regrading = "no"
